717a8d57f804987ed60bff80c942f3aba50ba707
yashrawat <yashrawat682@gmail.com>
2023-08-21 11:50:38 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home:  Scaffold(
+        appBar:AppBar(title: const Text("Chatting Room"),),
+      ),
+    );
+  }
+}
